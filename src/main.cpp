5a027d9e9870fa54f98044f5e08b511c4c583b4b
caseyfarina <caseyfarina@gmail.com>
2016-08-01 21:17:03 -0700

    initial commit
    setting things up

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include "ofMain.h"
+#include "ofApp.h"
+#include "ofAppGlutWindow.h"
+
+//========================================================================
+int main( ){
+    
+    ofAppGlutWindow window; // create a window
+    
+    // THIS IS THE IMPORTANT PART:
+    window.setGlutDisplayString("rgba alpha double stencil samples>=4");
+    
+    
+    // set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
+    ofSetupOpenGL(&window, 1024, 768, OF_WINDOW);
+    ofRunApp(new ofApp()); // start the app
+    
+    /*
+    
+    ofAppGlutWindow window;
+    window.setGlutDisplayString("rgba double samples>=4 depth");
+    ofSetupOpenGL(&window, 1024,768, OF_WINDOW);			// <-------- setup the GL context
+    
+    // this kicks off the running of my app
+    // can be OF_WINDOW or OF_FULLSCREEN
+    // pass in width and height too:
+    ofRunApp( new ofApp());
+     */
+}
---
+++ src/ofApp.cpp
@@ -0,0 +1,137 @@
+#include "ofApp.h"
+
+
+
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofSetFrameRate(100.0f);
+    ofSetCurveResolution(100);
+    
+       
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    
+    
+    ofMesh meshy;
+    meshy.setMode(OF_PRIMITIVE_TRIANGLE_STRIP);
+    
+    
+    float widthSmooth = 5;
+    float angleSmooth;
+    
+    for (int i = 0;  i < line.getVertices().size(); i++){
+        
+        
+        int me_m_one = i-1;
+        int me_p_one = i+1;
+        if (me_m_one < 0) me_m_one = 0;
+        if (me_p_one ==  line.getVertices().size()) me_p_one =  line.getVertices().size()-1;
+        
+        ofPoint diff = line.getVertices()[me_p_one] - line.getVertices()[me_m_one];
+        float angle = atan2(diff.y, diff.x);
+        
+        if (i == 0) angleSmooth = angle;
+        else {
+            
+            angleSmooth = ofLerpDegrees(angleSmooth, angle, 1.0);
+            
+        }
+        
+        
+        float dist = diff.length();
+        //ofSeedRandom(4);
+        float w = ofMap(ofNoise(ofGetFrameNum()*0.002f,i*0.01),0,1,0,0.4);
+        
+        //float w = ofMap(dist, 0, 5, 3, .1, true);
+        
+        
+        
+        //widthSmooth = 0.9f * widthSmooth + w ;
+        
+        if (i == 0 || i == line.getVertices().size()) widthSmooth = 0;
+        else{
+            widthSmooth = 0.9 * widthSmooth + w;
+        }
+        
+        
+        
+        ofPoint offset;
+        offset.x = cos(angleSmooth + PI/2) * widthSmooth;
+        offset.y = sin(angleSmooth + PI/2) * widthSmooth;
+        
+        
+        
+        meshy.addVertex(  line.getVertices()[i] +  offset + (w *3));
+        meshy.addVertex(  line.getVertices()[i] -  offset + (w*3));
+        
+        
+        
+    }
+    
+    ofSetColor(0,0,0);
+    meshy.draw();
+    //ofSetColor(100,100,100);
+    //meshy.drawWireframe();
+    
+   
+    
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    if (key == 'f') ofToggleFullscreen();
+    
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    
+    line.curveTo(ofPoint(x,y));
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    
+    line.clear();
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
+}
